91960d151be67de1e794ce6826b9c3f486c46548
jlchen5 <jl_chen1@outlook.com>
2024-04-22 11:19:11 +0200

    Create __init__.py

---
+++ fmd5sum/__init__.py
@@ -0,0 +1,8 @@
+from .fmd5sum import md5sum, main
+
+__all__ = ["md5sum", "main"]
+
+# You can also add package-wide constants or configuration options here
+__version__ = "0.1.0"
+__author__ = "Jiale Chen"
+__email__ = "xomics1@gmail.com"
